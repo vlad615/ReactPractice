196936b31a812046952d4a2b339447c959e15b16
VladislavAmbrosov <ambrosov413@gmail.com>
2026-04-17 20:36:19 +0300

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { ToDoList, type ListProps } from "./ToDoList"
 import './App.css';
 import { HW1 } from './HW/1/HW1';
-import { useState } from "react";
+// import { useState } from "react";
 import { HW2 } from './HW/2/HW2';
 // import { HW3 } from './HW/3/HW3';
 // import { HW4 } from './HW/4/HW4';
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,89 +1,89 @@
-import { useRef, useState } from "react"
-import { Button } from "./Buttons"
+// import { useRef, useState } from "react"
+// import { Button } from "./Buttons"
 
 
-type TaskProps = {
-    id: string
-    title: string
-    isDone: boolean
-}
+// type TaskProps = {
+//     id: string
+//     title: string
+//     isDone: boolean
+// }
 
-export type ListProps = {
-    id: string
-    title: string
-    tasks: TaskProps[]
-    // updateData: (data: ListProps)=>void
-}
+// export type ListProps = {
+//     id: string
+//     title: string
+//     tasks: TaskProps[]
+//     // updateData: (data: ListProps)=>void
+// }
 
-type Props = {
-    listTask: ListProps
-    updateData: (data: ListProps) => void
-}
+// type Props = {
+//     listTask: ListProps
+//     updateData: (data: ListProps) => void
+// }
 
-export function ToDoList({ listTask, updateData }: Props) {
-    const [currentList, setCurrentList] = useState(listTask.tasks)
-    const [name, setName] = useState("");
-    const [currentFilter, setCurrentFilter] = useState("all")
+// export function ToDoList({ listTask, updateData }: Props) {
+//     const [currentList, setCurrentList] = useState(listTask.tasks)
+//     const [name, setName] = useState("");
+//     const [currentFilter, setCurrentFilter] = useState("all")
 
-    const TasksItems = currentList.length ? currentList.map(task => {
-        return (
-            <li key={task.id} >
-                <label>
-                    <input type="checkbox" checked={task.isDone} onChange={() => { changeDone(task.id) }} />
-                    {task.title}
-                </label><Button name="x" callBack={() => delTask(task.id)} />
-            </li>)
-    }) : <p>List is empty</p>
+//     const TasksItems = currentList.length ? currentList.map(task => {
+//         return (
+//             <li key={task.id} >
+//                 <label>
+//                     <input type="checkbox" checked={task.isDone} onChange={() => { changeDone(task.id) }} />
+//                     {task.title}
+//                 </label><Button name="x" callBack={() => delTask(task.id)} />
+//             </li>)
+//     }) : <p>List is empty</p>
 
-    function filterList(typeFileter?: "active" | "complited") {
-        typeFileter === "active"
-            ? setCurrentList(listTask.tasks.filter(task => !task.isDone))
-            : setCurrentList(listTask.tasks.filter(task => task.isDone))
-    }
+//     function filterList(typeFileter?: "active" | "complited") {
+//         typeFileter === "active"
+//             ? setCurrentList(listTask.tasks.filter(task => !task.isDone))
+//             : setCurrentList(listTask.tasks.filter(task => task.isDone))
+//     }
 
-    function addTask() {
-        if (name) {
-            let newState = [...listTask.tasks, { id: crypto.randomUUID(), title: name, isDone: false }]
-            setCurrentList(newState)
-            setName("")
-            console.log(name);
+//     function addTask() {
+//         if (name) {
+//             let newState = [...listTask.tasks, { id: crypto.randomUUID(), title: name, isDone: false }]
+//             setCurrentList(newState)
+//             setName("")
+//             console.log(name);
             
 
-            console.log(newState);
+//             console.log(newState);
 
-            updateData({ id: listTask.id, title: listTask.title, tasks: newState })
-        }
+//             updateData({ id: listTask.id, title: listTask.title, tasks: newState })
+//         }
 
-    }
+//     }
 
-    function delTask(idTask: string) {
-        let newState = listTask.tasks.filter(task => task.id !== idTask)
-        setCurrentList(newState)
-        console.log(newState);
+//     function delTask(idTask: string) {
+//         let newState = listTask.tasks.filter(task => task.id !== idTask)
+//         setCurrentList(newState)
+//         console.log(newState);
 
-        updateData({ id: listTask.id, title: listTask.title, tasks: newState })
-    }
+//         updateData({ id: listTask.id, title: listTask.title, tasks: newState })
+//     }
 
-    function changeDone(idTask: string) {
-        let newState = listTask.tasks.map(task => task.id === idTask ? { ...task, isDone: !task.isDone } : task)
-        setCurrentList(newState)
-        console.log(newState);
+//     function changeDone(idTask: string) {
+//         let newState = listTask.tasks.map(task => task.id === idTask ? { ...task, isDone: !task.isDone } : task)
+//         setCurrentList(newState)
+//         console.log(newState);
 
-        updateData({ id: listTask.id, title: listTask.title, tasks: newState })
-    }
+//         updateData({ id: listTask.id, title: listTask.title, tasks: newState })
+//     }
 
 
-    return (
-        <div>
-            <h2>{listTask.title}</h2>
-            <input required={true} type="text" placeholder="New task" onChange={(ev)=>setName(ev.target.value)} /><Button name="+" callBack={() => addTask()} />
-            <ul>
-                {TasksItems}
-            </ul>
-            <Button name="All" callBack={() => { setCurrentList(listTask.tasks) }} />
-            <Button name="Active" callBack={() => { filterList("active") }} />
-            <Button name="Complited" callBack={() => { filterList("complited") }} />
-        </div>
+//     return (
+//         <div>
+//             <h2>{listTask.title}</h2>
+//             <input required={true} type="text" placeholder="New task" onChange={(ev)=>setName(ev.target.value)} /><Button name="+" callBack={() => addTask()} />
+//             <ul>
+//                 {TasksItems}
+//             </ul>
+//             <Button name="All" callBack={() => { setCurrentList(listTask.tasks) }} />
+//             <Button name="Active" callBack={() => { filterList("active") }} />
+//             <Button name="Complited" callBack={() => { filterList("complited") }} />
+//         </div>
 
-    )
-}
+//     )
+// }
