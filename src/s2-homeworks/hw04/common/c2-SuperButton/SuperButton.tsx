6bc90260d707fb051096c55ecdcb297cee1bbe4e
VladislavAmbrosov <ambrosov413@gmail.com>
2026-07-09 11:03:47 +0300

    solving problem with className

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -20,7 +20,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     const finalClassName = s.button
         + (disabled? ' ' + s.disabled : '') 
         + (xType === 'red' ? ' ' + s.red : '') 
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (className ? ' ' + s[className] : '') // задачка на смешивание классов
 
     return (
         <button
