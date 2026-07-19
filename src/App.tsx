f7bdf5c16faf12286c5e5802946a65a89c133681
VladislavAmbrosov <ambrosov413@gmail.com>
2026-07-19 20:13:13 +0300

    hw 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { HW4 as TS4 } from './HW/4/HW4';
-import { Counter } from "./HW/Counter";
 import HW5 from './s2-homeworks/hw05/HW5';
 import HW6 from './s2-homeworks/hw06/HW6';
 
--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import { restoreState, saveState } from './localStorage/localStorage'
+import { useState } from 'react'
 import s2 from '../../App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import s from './HW6.module.css'
+import { restoreState, saveState } from './localStorage/localStorage'
 
 /*
  * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
