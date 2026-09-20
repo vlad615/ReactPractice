1c7ac1e8a8bd3f1dc4d0d42674d809d218cb9b65
Vlad <ambrosov@413gmail.com>
2026-09-20 12:06:13 +0300

    hw 15 pagination

--- src/features/s2/ui/main/pages/JuniorPlus.tsx
+++ src/features/s2/ui/main/pages/JuniorPlus.tsx
@@ -3,7 +3,7 @@ import HW11 from './junior-plus/hw11/HW11'
 import HW12 from './junior-plus/hw12/HW12'
 import HW13 from './junior-plus/hw13/HW13'
 import HW14 from './junior-plus/hw14/HW14'
-// import HW15 from './junior-plus/hw15/HW15'
+import HW15 from './junior-plus/hw15/HW15'
 
 function JuniorPlus() {
     return (
@@ -13,7 +13,7 @@ function JuniorPlus() {
             <HW12 />
             <HW13 />
             <HW14 />
-            {/* <HW15 /> */}
+            <HW15 />
         </div>
     )
 }
--- src/features/s2/ui/main/pages/junior-plus/hw15/common/c10-SuperSort/test/pureChange.test.tsx
+++ src/features/s2/ui/main/pages/junior-plus/hw15/common/c10-SuperSort/test/pureChange.test.tsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import { pureChange } from '../SuperSort'
-// import { expect, test } from 'vitest'
+import React from 'react'
+import { pureChange } from '../SuperSort'
+import { expect, test } from 'vitest'
 
-// test('sort ""', () => {
-//     const newSort = pureChange('', '1a', '0a')
-//     expect(newSort).toBe('1a')
-// })
-// test('sort "1a"', () => {
-//     const newSort = pureChange('1a', '1a', '0a')
-//     expect(newSort).toBe('0a')
-// })
-// test('sort "0a"', () => {
-//     const newSort = pureChange('0a', '1a', '0a')
-//     expect(newSort).toBe('')
-// })
-// test('sort "1b"', () => {
-//     const newSort = pureChange('1b', '1a', '0a')
-//     expect(newSort).toBe('1a')
-// })
+test('sort ""', () => {
+    const newSort = pureChange('', '1a', '0a')
+    expect(newSort).toBe('1a')
+})
+test('sort "1a"', () => {
+    const newSort = pureChange('1a', '1a', '0a')
+    expect(newSort).toBe('0a')
+})
+test('sort "0a"', () => {
+    const newSort = pureChange('0a', '1a', '0a')
+    expect(newSort).toBe('')
+})
+test('sort "1b"', () => {
+    const newSort = pureChange('1b', '1a', '0a')
+    expect(newSort).toBe('1a')
+})
